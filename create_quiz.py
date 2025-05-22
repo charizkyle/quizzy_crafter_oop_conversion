0db329fa55e3e4cdc129e6cc538a6435c12c1c3e
charizkyle <santoscharizkyle@gmail.com>
2025-05-22 17:54:32 +0800

    deleted enter questions screen and added it to create quiz file

--- create_quiz.py
+++ create_quiz.py
@@ -35,3 +35,55 @@ class CreateQuizScreen:
         ).place(x=300, y=430)
 
         self.app.switch_frame(frame)
+
+    def create_question_entry_screen(self):
+        frame = tk.Frame(self.root)
+        tk.Label(frame, image=self.app.create_bg).place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.entries = []
+        labels = [
+            "Question", "Option A", "Option B", "Option C", "Option D", "Correct Answer \n(a/b/c/d)"
+        ]
+        y_positions = [150, 210, 260, 310, 360, 410]
+
+        for idx, text in enumerate(labels):
+            tk.Label(frame, text=text, font=self.app.custom_font, bg="#004477", fg="white").place(x=20, y=y_positions[idx])
+            entry = tk.Entry(frame, width=50, font=self.app.custom_font, bg="#004477", fg="light pink")
+            entry.place(x=200, y=y_positions[idx])
+            self.entries.append(entry)
+
+        def add_question():
+            question_text = self.entries[0].get()
+            options = [self.entries[idx].get() for idx in range(1, 5)]
+            correct_answer = self.entries[5].get().lower()
+            if question_text and all(options) and correct_answer:
+                self.quiz_manager.questions.append({
+                    "question": question_text,
+                    "options": options,
+                    "answer": correct_answer
+                })
+                for entry in self.entries:
+                    entry.delete(0, tk.END)
+
+        def save():
+            add_question()
+            self.quiz_manager.save_quiz()
+            self.app.load_start_menu()
+
+        tk.Button(
+            frame,
+            image=self.app.button_images["add_question"],
+            command=lambda: [self.app.play_click_sound(), add_question()],
+            borderwidth=0,
+            bg="#1f628e"
+        ).place(x=100, y=500)
+
+        tk.Button(
+            frame,
+            image=self.app.button_images["save"],
+            command=lambda: [self.app.play_click_sound(), save()],
+            borderwidth=0,
+            bg="#1f628e"
+        ).place(x=500, y=500)
+
+        self.app.switch_frame(frame)
