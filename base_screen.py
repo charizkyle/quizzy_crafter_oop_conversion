1b919d44fe54b645bf1fa1310acbf5748724a764
charizkyle <santoscharizkyle@gmail.com>
2025-05-22 17:36:34 +0800

    cannot import CreateQuizScreen, changed base screen

--- base_screen.py
+++ base_screen.py
@@ -4,17 +4,44 @@ from PIL import Image, ImageTk
 import winsound
 import os
 
-class BaseScreen(tk.Frame):
-    def __init__(self, master, app, bg_image_path=None):
-        super().__init__(master)
-        self.master = master
-        self.app = app
+class BaseScreen:
+    def __init__(self, root):
+        self.root = root
+        self.current_frame = None
         self.custom_font = tkFont.Font(family="consolas", size=14)
-        if bg_image_path:
-            bg_image = ImageTk.PhotoImage(Image.open(bg_image_path).resize((800, 600)))
-            self.bg_label = tk.Label(self, image=bg_image)
-            self.bg_label.image = bg_image  # Keep a reference
-            self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        # Load Images
+        self.start_bg = self.load_background("assets/start_bg.png")
+        self.create_bg = self.load_background("assets/create_bg.png")
+        self.take_bg = self.load_background("assets/take_bg.png")
+        self.score_bg = self.load_background("assets/score_bg.png")
+
+        # Load Button Images
+        self.button_images = self.load_button_images()
+
+    def switch_frame(self, new_frame):
+        if self.current_frame:
+            self.current_frame.destroy()
+        self.current_frame = new_frame
+        self.current_frame.pack(fill="both", expand=True)
+
+    def load_background(self, path):
+        return ImageTk.PhotoImage(Image.open(path).resize((800, 600)))
+
+    def load_button_image(self, path):
+        img = Image.open(path).convert("RGBA")
+        return ImageTk.PhotoImage(img.resize((200, 60)))
+
+    def load_button_images(self):
+        return {
+            "create": self.load_button_image("assets/create_button.png"),
+            "take": self.load_button_image("assets/take_button.png"),
+            "next": self.load_button_image("assets/next_button.png"),
+            "add_question": self.load_button_image("assets/add_question_button.png"),
+            "save": self.load_button_image("assets/save_button.png"),
+            "back": self.load_button_image("assets/back_button.png"),
+            "submit": self.load_button_image("assets/submit_button.png"),
+        }
 
     def play_click_sound(self):
         winsound.PlaySound("assets/click.wav", winsound.SND_FILENAME)
