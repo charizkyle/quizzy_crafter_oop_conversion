be9d2aa40166f465af564f90a2db8fe75ad64270
charizkyle <santoscharizkyle@gmail.com>
2025-05-22 16:04:54 +0800

    store result in JSON file through quiz manager

--- gui/screens/result_screen.py
+++ gui/screens/result_screen.py
@@ -11,3 +11,10 @@ class ResultScreen(tk.Frame):
                  font=("consolas", 16),
                  fg="white", bg="#e88e93"
                  ).place(x=310, y=50)
+        
+        # store result JSON file (already saved by QuizManager)
+        tk.Button(self, image=app.assets.buttons["submit"],
+                  borderwidth=0, bg="#1f628e",
+                  command=lambda: [app.assets.play_click(),
+                                   self.app.show_start_screen()]
+                 ).place(x=300, y=500)
